cb8ecfbc77a2cef7b325a4b835ee75840d839c47
victor840 <victor.ugoanusi@gmail.com>
2021-01-31 10:22:50 -0500

    Initial Project Add

---
+++ mazi-pet-clinic/src/main/java/victorspringFramework/mazipetclinic/MaziPetClinicApplication.java
@@ -0,0 +1,13 @@
+package victorspringFramework.mazipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaziPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaziPetClinicApplication.class, args);
+	}
+
+}
---
+++ mazi-pet-clinic/src/test/java/victorspringFramework/mazipetclinic/MaziPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package victorspringFramework.mazipetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaziPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
